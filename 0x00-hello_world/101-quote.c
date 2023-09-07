5e13cd1f52c76df1a24d1fc4a62db27b7b857ac7
ibtider <btissamderouich33@gmail.com>
2023-09-07 16:29:29 -0700

    updat

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
